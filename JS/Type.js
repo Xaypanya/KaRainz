293894e16794f41426056cff63697d79d72d8e61
octopi <dackogan@gmail.com>
2022-03-07 02:20:14 +0700

    first commit

---
+++ JS/Type.js
@@ -0,0 +1,13 @@
+
+    var typed = new Typed('#type', {
+    // Waits 1000ms after typing "First"
+    strings: ['SABAIDEE', 'KONNICHIWA','XIN CHAO','SAWASDEE','NI HAO','ANNYEONG HASEYO',"WHAT'S UP"],
+    smartBackspace: true,
+    fadeOut: true,
+    loop: true,
+    typeSpeed: 100,
+    backSpeed: 100,
+    startDelay: 100,
+  });
+  
+
